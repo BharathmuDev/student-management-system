094bce16ef69c2b9f2b7ececdbb8914e8e0d58c5
BharathmuDev <bharathmogaveer01@gmail.com>
2023-11-09 23:08:15 -0800

    login for teachers login

--- teacherLogin.js
+++ teacherLogin.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const loginForm = document.querySelector(".login-form"); 
+    const loginForm = document.querySelector(".login-form");
     loginForm.addEventListener("submit", function(event) {
         event.preventDefault();
 
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
         const password = document.getElementById("password").value;
 
         if (username === "teacher" && password === "password") {
+            
             window.location.href = "teacherDashboard.html";
         } else {
             alert("Invalid username or password. Please try again.");
